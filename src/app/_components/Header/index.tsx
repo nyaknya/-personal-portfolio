9910cca47dc9aea9af46571e4fe814f5be7b4042
심은주 <nyaknya@naver.com>
2024-12-18 18:45:17 +0900

    ✨ feat: 네비게이션 추가

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -3,6 +3,7 @@
 import style from "./Header.module.css";
 import Image from "next/image";
 import { motion, useScroll } from "framer-motion";
+import Link from "next/link";
 
 export default function Header() {
   const { scrollYProgress } = useScroll();
@@ -18,6 +19,24 @@ export default function Header() {
       <h1>
         <Image src="/images/logo.svg" alt="로고" width={70} height={62} />
       </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="#">Intro</Link>
+          </li>
+          <li>
+            <Link href="#">About</Link>
+          </li>
+          <li>
+            <Link href="#">Project</Link>
+          </li>
+          <li>
+            <Link href="#" className={style.contact}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
