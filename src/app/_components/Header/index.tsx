2b5cb17f1de0458a39902bec56f18760d55fc357
심은주 <nyaknya@naver.com>
2024-12-18 18:16:41 +0900

    ✨ feat: 헤더 스크롤 프로그래스바 추가

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -2,10 +2,19 @@
 
 import style from "./Header.module.css";
 import Image from "next/image";
+import { motion, useScroll } from "framer-motion";
 
 export default function Header() {
+  const { scrollYProgress } = useScroll();
+
   return (
     <header className={style.header}>
+      <motion.div
+        className={style.progress}
+        style={{
+          scaleX: scrollYProgress,
+        }}
+      ></motion.div>
       <h1>
         <Image src="/images/logo.svg" alt="로고" width={70} height={62} />
       </h1>
